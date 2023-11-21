83ed20d7d7bafae86022b015f98c5d14216c9bac
PiyushInnotech <piyushmangla@innotechteam.in>
2023-11-21 19:01:48 +0530

    Todo app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Todo } from './Todo/Todo'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+function App() {
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
