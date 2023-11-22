4983574eaf5632276d29d3b017040e48ed79ae7d
amritSingh <amritsingh@innotechteam.in>
2023-11-22 13:12:31 +0530

    remove find method from handleEditChangeFile and pass directly whole item

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -48,11 +48,8 @@ export const Todo = () => {
         setShowItem([...data])
     }
 
-    const handleEditChange = (index) => {
-        const currentTask = showItem.find((item, ind) => {
-            return ind === index
-        })
-        setInput({ item: currentTask.item })
+    const handleEditChange = (element, index) => {
+        setInput({ item: element.item })
         setEdit(index)
     }
     return (
@@ -72,7 +69,7 @@ export const Todo = () => {
                             <div className="d-flex align-items-center">
                                 {element.isComplete ? <span className="border mt-1 rounded-pill text-center bg-success text-white py-1 px-2">Completed</span> : ''}
                                 <div className="ml-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 512 512" onClick={() => handleEditChange(index)}><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" /></svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 512 512" onClick={() => handleEditChange(element, index)}><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" /></svg>
                                 </div>
                             </div>
 
